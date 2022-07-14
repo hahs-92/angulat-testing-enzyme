87db89cd1b7d057e89865317cdfe10f03738088c
hahs <alexandre_2420@outlook.com>
2022-07-14 15:43:55 -0500

    [added]: mocha report

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,39 +1,43 @@
 import { Calculator } from './calculator';
 
 describe('Tets for calculator', () => {
-  it('#multiply 3 * 3 should return 9 ', () => {
-    //arrange
-    const calculator = new Calculator();
-    //act
-    const rta = calculator.multiply(3, 3);
-    //assert
-    expect(rta).toEqual(9);
+  describe('Test for multiply', () => {
+    it('#multiply 3 * 3 should return 9 ', () => {
+      //arrange
+      const calculator = new Calculator();
+      //act
+      const rta = calculator.multiply(3, 3);
+      //assert
+      expect(rta).toEqual(9);
+    });
+
+    it('#multiply 3 * 0 should return 0', () => {
+      //arrange
+      const calculator = new Calculator();
+      //act
+      const rta = calculator.multiply(3, 0);
+      //assert
+      expect(rta).toEqual(0);
+    });
   });
 
-  it('#multiply 3 * 0 should return 0', () => {
-    //arrange
-    const calculator = new Calculator();
-    //act
-    const rta = calculator.multiply(3, 0);
-    //assert
-    expect(rta).toEqual(0);
-  });
+  describe('Test for divide', () => {
+    it('#divide 12 / 4 should return 3', () => {
+      //arrange
+      const calculator = new Calculator();
+      //act
+      const rta = calculator.divide(12, 4);
+      //assert
+      expect(rta).toEqual(3);
+    });
 
-  it('#divide 12 / 4 should return 3', () => {
-    //arrange
-    const calculator = new Calculator();
-    //act
-    const rta = calculator.divide(12, 4);
-    //assert
-    expect(rta).toEqual(3);
-  });
-
-  it('#divide 12 / 0 should return null', () => {
-    //arrange
-    const calculator = new Calculator();
-    //act
-    const rta = calculator.divide(12, 0);
-    //assert
-    expect(rta).toBeNull();
+    it('#divide 12 / 0 should return null', () => {
+      //arrange
+      const calculator = new Calculator();
+      //act
+      const rta = calculator.divide(12, 0);
+      //assert
+      expect(rta).toBeNull();
+    });
   });
 });
