ceef6d6d5a7da17dc67df738a0d3a1d9ba60968c
hahs <alexandre_2420@outlook.com>
2022-07-14 15:29:28 -0500

    [added]: coverage

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,12 +1,39 @@
 import { Calculator } from './calculator';
 
 describe('Tets for calculator', () => {
-  fit('#multiply should return 9 ', () => {
+  it('#multiply 3 * 3 should return 9 ', () => {
     //arrange
     const calculator = new Calculator();
     //act
     const rta = calculator.multiply(3, 3);
     //assert
     expect(rta).toEqual(9);
+  });
+
+  it('#multiply 3 * 0 should return 0', () => {
+    //arrange
+    const calculator = new Calculator();
+    //act
+    const rta = calculator.multiply(3, 0);
+    //assert
+    expect(rta).toEqual(0);
+  });
+
+  it('#divide 12 / 4 should return 3', () => {
+    //arrange
+    const calculator = new Calculator();
+    //act
+    const rta = calculator.divide(12, 4);
+    //assert
+    expect(rta).toEqual(3);
+  });
+
+  it('#divide 12 / 0 should return null', () => {
+    //arrange
+    const calculator = new Calculator();
+    //act
+    const rta = calculator.divide(12, 0);
+    //assert
+    expect(rta).toBeNull();
   });
 });
