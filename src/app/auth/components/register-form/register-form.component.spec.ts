b0a395010a3c6bbcf62bf5b3186a43a29dc7df11
hahs <alexandre_2420@outlook.com>
2022-10-20 09:45:27 -0500

    added: test for register component

--- src/app/auth/components/register-form/register-form.component.spec.ts
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -12,7 +12,10 @@ fdescribe('RegisterFormComponent', () => {
 
   beforeEach(async () => {
     //creamos el spy. create es el metodo a espiar
-    const spy = jasmine.createSpyObj('UsersService', ['create']);
+    const spy = jasmine.createSpyObj('UsersService', [
+      'create',
+      'isAvailableByEmail',
+    ]);
 
     await TestBed.configureTestingModule({
       declarations: [RegisterFormComponent],
@@ -29,5 +32,53 @@ fdescribe('RegisterFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should the emailFiled be invalid', () => {
+    //component.form.get('email')?.setValue('esto no es un correo');
+    //hago uso de los getters
+    component.emailField?.setValue('esto no es un correo valido');
+
+    //expect(component.form.get('email')?.invalid).toBeTruthy();
+    //uso de los getters del componente
+    expect(component.emailField?.invalid).withContext('empty').toBeTruthy();
+
+    //el context nos sirve para cuando tenemos varios expects
+    component.emailField?.setValue('');
+    expect(component.emailField?.invalid)
+      .withContext('wrong email')
+      .toBeTruthy();
+  });
+
+  it('should the passwordField be invalid', () => {
+    const wrongPassword = 'wrong password';
+    const rightPassword = '123456';
+
+    component.passwordField?.setValue('');
+    expect(component.passwordField?.invalid).withContext('empty').toBeTruthy();
+
+    component.passwordField?.setValue(wrongPassword);
+    expect(component.passwordField?.invalid)
+      .withContext('without numbers')
+      .toBeTruthy();
+
+    component.passwordField?.setValue(rightPassword);
+    expect(component.passwordField?.valid)
+      .withContext('right password')
+      .toBeTruthy();
+  });
+
+  it('should the form be invalid', () => {
+    const userMock = {
+      name: 'Alex',
+      email: 'Hernandez',
+      password: '',
+      confirmPassword: '',
+      checkTerms: false,
+    };
+
+    component.form.patchValue(userMock);
+
+    expect(component.form.invalid).toBeTruthy;
   });
 });
